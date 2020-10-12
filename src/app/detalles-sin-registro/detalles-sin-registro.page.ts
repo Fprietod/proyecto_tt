a993950d0bb1d56d85cc50bdbc7d5eaac2e3bc43
F.Prieto <felipe00@live.com.mx>
2020-10-11 22:15:26 -0500

    Modulo reporte sin foto funcionando

--- src/app/detalles-sin-registro/detalles-sin-registro.page.ts
+++ src/app/detalles-sin-registro/detalles-sin-registro.page.ts
@@ -14,12 +14,35 @@ export class DetallesSinRegistroPage implements OnInit {
 
 	public reporte: Observable<reporte>;
 	public reportId: string = this.route.snapshot.paramMap.get('id');
+	public incidente_falso : number = 0;
+	public contador: number = 0;
+	public incidente_verdadero: number = 0;
+	public contador_verdadero: number = 0;
+	public respuesta : string
 
+  
   constructor(private firestoreService: FirestoreService, private route: ActivatedRoute, alertController: AlertController, private router : Router) { }
 
   ngOnInit() {
   const reportId: string = this.route.snapshot.paramMap.get('id');
   this.reporte = this.firestoreService.getReportDetail(reportId).valueChanges();
+  
   }
+   confirma(){
+   this.contador_verdadero = this.incidente_verdadero
+   this.contador_verdadero = this.contador_verdadero+=1;
+   this.incidente_verdadero = this.contador_verdadero;
+   if(this.incidente_verdadero >= 4){
+   this.respuesta = "Este reporte es confiable";
+   }
+   }
 
+   falso2(){
+    this.contador=this.incidente_falso;
+   this.contador = this.contador+=1;
+   this.incidente_falso = this.contador;
+ 	if(this.incidente_falso == 3){
+ 	this.firestoreService.deleteReport(this.reportId)
+ 	}
+   }
 }
